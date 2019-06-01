0440ae19b77f150074196d577caf2d7eb5ef6438
Noteworthy Online <online@noteworthysoftware.com>
2019-06-01 07:48:06 -0400

    First try at ios build

--- src/vlp-cordova.js
+++ src/vlp-cordova.js
@@ -1,22 +1,22 @@
-
 import './cordova.html';
 import {vlpMap} from './vlp.js';
 
 var app = {
     initialize: function() {
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-        document.addEventListener('pause', this.onPause.bind(this), false);
-        document.addEventListener('resume', this.onResume.bind(this), false);
+        //document.addEventListener('pause', this.onPause.bind(this), false);
+        //document.addEventListener('resume', this.onResume.bind(this), false);
     },
 
     onDeviceReady: function() {
+        //StatusBar.overlaysWebView(false);
         vlpMap(false);
     },
     onPause: function() {
-        console.warn('-----------------PAUSE----------------');
+        //console.warn('-----------------PAUSE----------------');
     },
     onResume: function() {
-        console.warn('-----------------RESUME---------------');
+        //console.warn('-----------------RESUME---------------');
     },
 };
 
