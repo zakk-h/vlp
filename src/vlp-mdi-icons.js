dedd92abc37962016981a4173ea4d8d45dd03489
zakk-h <zakk@valdese.net>
2020-05-29 13:04:13 -0400

    Finished Point of Interest Header with icons and coordinates to not interfere with others nearby

--- src/vlp-mdi-icons.js
+++ src/vlp-mdi-icons.js
@@ -1,4 +1,52 @@
+import {mdiTableFurniture, mdiFlagTriangle, mdiCamera, mdiCarMultiple, mdiParking, 
+	mdiSailBoat, mdiBridge, mdiNature, mdiInformationOutline, mdiTableChair, mdiWaves, mdiBike,
+	mdiFish} from '@mdi/js';
+
+// https://materialdesignicons.com
+
+const mdiIcon = {
+    bike:   mdiBike,
+    boat:	mdiSailBoat,
+	bridge:	mdiBridge,
+	camera:	mdiCamera,
+	cars:	mdiCarMultiple,
+	fish:	mdiFish,
+	flag:	mdiFlagTriangle,
+	info:	mdiInformationOutline,
+	nature:	mdiNature,
+	parking: mdiParking,
+	picnic:	mdiTableFurniture,
+	tablechair: mdiTableChair,
+	waves:	mdiWaves
+}
+
+const mdiIconColor = {
+    bike:   '#000000',
+    boat:	'#A52A2A',
+	bridge:	'#000000',
+	camera:	'#040421',
+	cars:	'#800000',
+	fish:	'#F6E35B', 
+	flag:	'#EF6C00',
+	info:	'#0000AA',
+	nature:	'#00AA00',
+	parking: '#000000',
+	picnic:	'#5C2F00', 
+	tablechair: '#000000',
+	waves:	'#000066'
+}
+const mdiIconOutline = {
+    fish:	'#000000',
+    bridge:	'#000000'
+}
+const mdiIconOutlineWidth = {
+    fish:	'1.5',
+    bridge: '.75'
+}
+export {mdiIcon, mdiIconColor, mdiIconOutline, mdiIconOutlineWidth};
+/*
 import {mdiTableFurniture as mdi_Picnic, mdiFlagTriangle as mdi_Orienteering} from '@mdi/js';
 import {mdiCamera as mdi_Camera} from '@mdi/js';
 import {mdiFish as mdi_Fish} from '@mdi/js';
 export {mdi_Picnic,mdi_Orienteering, mdi_Camera, mdi_Fish};
+*/
