5e31b7a64175a29f7ad7f231281b08e0de8dd113
zakk-h <zakk@valdese.net>
2020-05-29 13:05:42 -0400

    Small fixes

--- src/vlp-mdi-icons.js
+++ src/vlp-mdi-icons.js
@@ -44,9 +44,3 @@ const mdiIconOutlineWidth = {
     bridge: '.75'
 }
 export {mdiIcon, mdiIconColor, mdiIconOutline, mdiIconOutlineWidth};
-/*
-import {mdiTableFurniture as mdi_Picnic, mdiFlagTriangle as mdi_Orienteering} from '@mdi/js';
-import {mdiCamera as mdi_Camera} from '@mdi/js';
-import {mdiFish as mdi_Fish} from '@mdi/js';
-export {mdi_Picnic,mdi_Orienteering, mdi_Camera, mdi_Fish};
-*/
