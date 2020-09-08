5dd34e6ebd4ee28c3c69051abca5106115847da4
Noteworthy Software, Inc <online@noteworthysoftware.com>
2020-09-08 03:01:34 -0400

    Switching to custom markdown loader
    The cloud offering of  markdown-loader used an old version
    of `markdown.js`, so this change had to be done. This will
    allow better link handling once we move to a link/route
    strategy for pages in the app.

---
+++ loader/markdown.js
@@ -0,0 +1,27 @@
+const marked = require("marked");
+const {cleanUrl,escape} = require('marked/src/helpers.js');
+
+marked.use({ renderer: {
+	link(href, title, text) {
+		href = cleanUrl(this.options.sanitize, this.options.baseUrl, href);
+		if (href === null) {
+			return text;
+		}
+		let out = '<a href="' + escape(href) + '"';
+		if (title) {
+			out += ' title="' + title + '"';
+		}
+		if (/^http/.test(href)) {
+			out += ' target="_blank"';
+		} else {
+			out += ' data-navigo';
+		}
+		out += '>' + text + '</a>';
+		return out;
+	}
+}});
+
+module.exports = function (markdown) {
+    this.cacheable();
+    return marked(markdown);
+};
