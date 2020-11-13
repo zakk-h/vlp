a34214cc4625011294162614600cbea0b79e471d
Noteworthy Software, Inc <online@noteworthysoftware.com>
2020-11-13 09:18:30 -0500

    Switch to dayjs (2.4k bundle size reduction)

--- src/whatsnew.js
+++ src/whatsnew.js
@@ -1,5 +1,8 @@
-import {formatDistance} from 'date-fns';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
 import {showModal} from './modal.js';
+
+dayjs.extend(relativeTime);
 
 export function showWhatsNew() {
 	let LatestWhatsNewEntry = vlpApp.appd.history[0][0];
@@ -16,7 +19,8 @@ export function showWhatsNew() {
 	ul.className = 'whatsnew';
 	vlpApp.appd.history.forEach(v => {
 		if (v[0] > lastSeen) {
-			let d = formatDistance(1000*Number(v[0]),Date.now(),{addSuffix:true});
+			let tstamp = 1000*Number(v[0]);
+			let d = dayjs(tstamp).fromNow();
 			let de = document.createElement('em');
 			let li = document.createElement('li');
 			de.innerText = d;
